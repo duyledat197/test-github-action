682c5a6624d5666b907307191db1a5d12cf147ee
thuocsi_datle <dat.le@thuocsi.vn>
2021-08-24 09:41:04 +0700

    test

--- test.bash
+++ test.bash
@@ -1,6 +1,10 @@
-  set -e
+set -e
 last_release=$(git branch --all --list '*release/20*' | sort -r | head -n 1 | cut -c18-)
+
+echo $GITHUB_BASE_REF
+echo $last_release
 if [[ $GITHUB_BASE_REF == $last_release ]];
 then
-    echo "ERROR: target branch is old (got \"${GITHUB_BASE_REF}\", expected \"${last_release}\")"; return 1
+    echo "ERROR: target branch is old (got \"${GITHUB_BASE_REF}\", expected \"${last_release}\")"; 
+    return 1
 fi
