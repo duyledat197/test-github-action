79cf7232957cd2cf97086f9b2a7be90d3ec223c7
thuocsi_datle <dat.le@thuocsi.vn>
2021-08-26 12:42:23 +0700

    test

--- test.go
+++ test.go
@@ -2,22 +2,93 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
+	"regexp"
+	"time"
 )
 
 const (
-	Test = "teset"
-	Ca   = "ca"
-	Baz  = "asd"
-	AC   = "ac"
-	B    = "asd"
-	C    = "asdas"
+	DATE_FMT = "20060102"
 )
+
+type ReleaseAssistant struct {
+	UserName  string
+	UserToken string
+
+	RgIssueKey     *regexp.Regexp
+	RepoBaseBranch map[string]string
+	ReleaseDay     time.Weekday
+
+	ConcurentLimit int
+}
+
+func (a *ReleaseAssistant) get(url string) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return a.call(url, req)
+}
+
+func (a *ReleaseAssistant) call(url string, req *http.Request) ([]byte, error) {
+	req.Header.Set("Accept", "application/json")
+	req.SetBasicAuth(a.UserName, a.UserToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+
+	}
+	return body, nil
+}
+
+func (a *ReleaseAssistant) searchRelease(when time.Time) (string, string, error) {
+	log.Println("searchRelease", when)
+
+	releaseDate := when.Format(DATE_FMT)
+	tmpl := url.QueryEscape(`project = LT AND summary ~ "` + releaseDate + `" AND issuetype = Release`)
+	resp, err := a.get(fmt.Sprintf("https://manabie.atlassian.net/rest/api/3/search?jql=%s", tmpl))
+	if err != nil {
+		return "", "", fmt.Errorf("error when fetching issues from search endpoint: %w", err)
+	}
+	log.Println(string(resp))
+	return "", "", nil
+}
 
 func main() {
 	jiraUserFlag := flag.String("user", "", "JIRA user name, eg: devops@manabie.com")
 	jiraTokenFlag := flag.String("token", "", "JIRA user token")
+	releaseDateFlag := flag.String("releaseDate", "", "Release date in yyyymmdd fmt, eg: 20160101")
 	log.Println("jiraUserFlag", *jiraUserFlag)
 	log.Println("jiraTokenFlag", *jiraTokenFlag)
+
+	a := &ReleaseAssistant{
+		UserName:  *jiraUserFlag,
+		UserToken: *jiraTokenFlag,
+
+		RgIssueKey: regexp.MustCompile(`LT-[0-9]{1,6}`),
+		RepoBaseBranch: map[string]string{
+			"manabie-com/backend":             "develop",
+			"manabie-com/school-portal-admin": "develop",
+			"manabie-com/student-app":         "develop",
+			"manabie-com/eibanam":             "develop",
+		},
+		ReleaseDay:     time.Thursday,
+		ConcurentLimit: 5,
+	}
+	if releaseDay, err := time.Parse(DATE_FMT, *releaseDateFlag); err == nil {
+		a.searchRelease(releaseDay)
+	}
 
 }
